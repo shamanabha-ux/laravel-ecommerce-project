b57d08516e6e59de85988045c3b89eb1b787413f
Shama <shamanabha@gmail.com>
2026-03-20 15:06:38 +0530

    Added authentication using Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    //return view('welcome');
-    return "Ecommerce project started";
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+   // return "The route is working!";
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
